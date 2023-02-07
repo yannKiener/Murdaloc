88a04919985e76eed14da5ce6ccc990762ab6483
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:45:54 +0100

    Fixed aggro

--- Assets/Scripts/Mob.cs
+++ Assets/Scripts/Mob.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Mob : MonoBehaviour {
 
+    bool inCombat = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,13 +13,48 @@ public class Mob : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        limitMovements();
 		
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if (collision.gameObject.tag == "Player") {
-			collision.gameObject.SendMessage ("enterCombat", this.gameObject);
+		if (collision.gameObject.tag == "Player" && !inCombat) {
+            inCombat = true;
+            Aggro(collision);
+            AggroAroundSelf(collision);
 		}
 		
 	}
+
+    private void limitMovements() {
+        if (inCombat)
+        {
+            Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+            pos.x = Mathf.Clamp01(pos.x);
+            pos.y = Mathf.Clamp01(pos.y);
+            transform.position = Camera.main.ViewportToWorldPoint(pos);
+        }
+    }
+
+    private void AggroAroundSelf(Collision2D collision)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), 5f);        
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            if (hitColliders[i].tag == "Enemy")
+            {
+                hitColliders[i].SendMessage("Aggro", collision);
+            }
+
+            i++;
+        }
+    }
+
+    private void Aggro(Collision2D collision)
+    {
+        inCombat = true;
+        collision.gameObject.SendMessage("enterCombat", this.gameObject);
+    }
 }
