4e5a20833c043d0e08a93bb604e2c450b49eb874
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:47:00 +0100

    Added interface as Prefab & updated MenuBar to open/close InGame menu

---
+++ Assets/InGameMenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InGameMenuButton : MonoBehaviourWithMouseOverColor, IPointerClickHandler
+{
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Interface.OpenCloseMenu();
+    }
+}
