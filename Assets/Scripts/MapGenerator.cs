b48ddaa7d0675273c27ee3b61d9edf83dd07ca83
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:45:50 +0100

    Map updates. Next step : add a gameobject to the otherSide of the generated map.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,17 +5,17 @@ using System;
 
 public class MapGenerator : MonoBehaviour {
 
-	public int startingLeftPoint;
 	public int width;
 
 	[Range(0,100)]
 	public int object1Density;
 	public string seed;
-	public bool useRandomSeed;	
+	public bool useRandomSeed;
+	public bool attachAtRight;
+	public GameObject attachAtObject;
 	public GameObject mapPrefab;
 	public GameObject object1Prefab;
 
-	//private int[] object1Map;
 
 	// Use this for initialization
 	void Start () {
@@ -23,16 +23,25 @@ public class MapGenerator : MonoBehaviour {
 	}
 
 	void GenerateMap() {
-		Vector3 originalPosition = mapPrefab.transform.position;
-		Vector3 originalScale = mapPrefab.transform.localScale;
+		
+		float startingX = 0;
+		float startingY = 0;
+		
+		if(attachAtRight){
+			startingX = getUpperXBound(attachAtObject) + (width/2);
+			startingY = attachAtObject.transform.position.y + attachAtObject.GetComponent<BoxCollider2D> ().offset.y + attachAtObject.GetComponent<BoxCollider2D> ().bounds.size.y / 2 - mapPrefab.GetComponent<Renderer> ().bounds.size.y*mapPrefab.GetComponent<BoxCollider2D> ().size.y/2;
+		} else {
+			startingX = getLowerXBound(attachAtObject) - (width/2);
+			startingY = attachAtObject.transform.position.y + attachAtObject.GetComponent<BoxCollider2D> ().offset.y + attachAtObject.GetComponent<BoxCollider2D> ().bounds.size.y / 2 - mapPrefab.GetComponent<Renderer> ().bounds.size.y*mapPrefab.GetComponent<BoxCollider2D> ().size.y/2;
+		}
+		//TODO trouver un moyen plus clean pour coller les deux BoxCollider2D ensemble ! :D Voir plus bas.
 
-		print("map position :" + mapPrefab.transform.position);
-		mapPrefab.transform.position += new Vector3((width/2) + startingLeftPoint, 0, 0);
-		mapPrefab.transform.localScale += new Vector3(width, 0, 0);
+		//float startingRightX = getLowerXBound(attachAtObject);
+
+		mapPrefab.transform.position = new Vector3(startingX, startingY, 0);
+		mapPrefab.transform.localScale = new Vector3(width, 1, 1);
 		Instantiate(mapPrefab);
 
-		mapPrefab.transform.position = originalPosition;
-		mapPrefab.transform.localScale = originalScale;
 		DrawObjectOnMap(object1Prefab, mapPrefab, object1Density);
 	}
 
@@ -44,12 +53,42 @@ public class MapGenerator : MonoBehaviour {
 
 		for (int i = 0; i < width; i++){
 			if(pseudoRandom.Next(0,100) < density){
-				float x = startingLeftPoint + i; //TODO :  trouver la position "gauche" autrement depuis la map en param
-				float y = map.transform.position.y + map.GetComponent<BoxCollider2D>().size.y/2 + objToDraw.GetComponent<Renderer>().bounds.size.y /2; //On veut que le pied de l'arbre soit en haut de la map..! Désolé pour la ligne dégueulasse :D
+				float x = getLowerXBound(map) + i;
+				float y = getUpperYBound(map) + objToDraw.GetComponent<Renderer>().bounds.size.y/2;
 				objToDraw.transform.position = new Vector3(x,y,0);
 				Instantiate(objToDraw); //draw at position x
 			}
 		}
+	}
+
+
+	//TODO Faire pareil avec les boxCollider2D a la place des Renderer ? Voir ligne dégueulasse au dessus.
+	private float getUpperYBound(GameObject obj){
+		return getBounds(obj,true,"y");
+	}
+	private float getLowerYBound(GameObject obj){
+		return getBounds(obj,false,"y");
+	}
+	private float getUpperXBound(GameObject obj){
+		return getBounds(obj,true,"x");
+	}
+	private float getLowerXBound(GameObject obj){
+		return getBounds(obj,false,"x");
+	}
+
+	private float getBounds(GameObject obj, Boolean upperRight, String coord){
+		float centerPosition = 0;
+		float halfSize = 0;
+
+		if (coord == "x"){
+			centerPosition = obj.transform.position.x;
+			halfSize = obj.GetComponent<Renderer>().bounds.size.x/2; 
+		} else if(coord == "y"){
+			centerPosition = obj.transform.position.y;
+			halfSize = obj.GetComponent<Renderer>().bounds.size.y/2;
+		}
+
+		return upperRight? centerPosition + halfSize : centerPosition - halfSize;
 	}
 	
 	// Update is called once per frame
