a5efd49f844122f55063d39ed1546c28fc782121
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:46:22 +0100

    Added Draggable class !

---
+++ Assets/Scripts/Interface/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public static GameObject currentItem;
+	private Vector3 startPosition;
+	
+	public void OnBeginDrag (PointerEventData eventData){
+		currentItem = gameObject;
+		startPosition = transform.position;
+
+	}
+
+	public void OnDrag (PointerEventData eventData){
+		transform.position = Input.mousePosition;
+
+	}
+
+	public void OnEndDrag (PointerEventData eventData){
+		currentItem = null;
+		transform.position = startPosition;
+
+	}
+
+
+
+	
+}
