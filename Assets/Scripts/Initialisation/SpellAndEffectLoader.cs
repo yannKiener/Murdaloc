a04d0c9e38d5cfaccd0870846c85252f8fbd6747
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:46:18 +0100

    Added sprint logo

--- Assets/Scripts/Initialisation/SpellAndEffectLoader.cs
+++ Assets/Scripts/Initialisation/SpellAndEffectLoader.cs
@@ -11,7 +11,7 @@ public class SpellAndEffectLoader : MonoBehaviour {
 		EffectsOnTime.Add (new EffectOnTime ("renovation","First HoT of the game",true,3,10,1,null,newHealOnTime(new Dictionary<Stat,float>{{Stat.intelligence,1.6f}},50)));
 		EffectsOnTime.Add (new EffectOnTime ("sprint","+60% moveSpeed",true,1,2,1,new StatEffect(new Dictionary<Stat,float>{{Stat.maxSpeed,60f}}),null));
 
-		Spells.Add (new HostileSpell ("fireball","A magic Fireball. That's it.", 10,2,0,5,5,newDamage(new Dictionary<Stat,float>{{Stat.intelligence,1.6f}},30),null,null));
+		Spells.Add (new HostileSpell ("fireball","A magic Fireball. That's it.", 10,2,0,0,5,newDamage(new Dictionary<Stat,float>{{Stat.intelligence,1.6f}},30),null,null));
 		Spells.Add (new HostileSpell ("slam","Slap your target's face with your first", 20,0,0,0,2,newDamage(new Dictionary<Stat,float>{{Stat.force,1f},{Stat.agility,0.5f}},30),null,null));
 		Spells.Add (new FriendlySpell ("renovation","FIRST HOTSPELL", 5,0.5f,0,0,5,null,new List<EffectOnTime>(),new List<EffectOnTime>{ EffectsOnTime.Get("renovation") }));
 		Spells.Add (new FriendlySpell ("sprint","60% moveSpeed for 2 seconds", 10,0,0,15,1,null,new List<EffectOnTime>(),new List<EffectOnTime>{ EffectsOnTime.Get("sprint") }));
