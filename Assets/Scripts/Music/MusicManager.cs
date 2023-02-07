fc4baad5148d583120d9fed1d5bd4f0dcf5ede8d
Yann Kiener <8420234+yannKiener@users.noreply.github.co>
2023-02-07 16:46:19 +0100

    Added beginning of Musics..

--- Assets/Scripts/Music/MusicManager.cs
+++ Assets/Scripts/Music/MusicManager.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour{
+	private static AudioClip music1;
+	private static AudioClip music2;
+	private static AudioSource source;
 
-	// Use this for initialization
-	void Start () {
-		
+	public static void playMusic(AudioClip music){
+		if (source == null) {
+			source = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<AudioSource> ();
+		}
+		if (music != source.clip) {
+			//StartCoroutine (fadeIn(1f));
+			//fade out, then new audioclip, then fadeIn.
+		} else {
+			//fade in
+		}
+		source.clip = music;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private static IEnumerator fadeIn(float time){
+		return null;
+	}
+
+	private static IEnumerator fadeOut(float time){
+		return null;
 	}
 }
